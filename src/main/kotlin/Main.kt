7701741e5500549ab5b216875e4cc0d4a7921f0f
Zuenah Wairimu <mnachariazuenah@gmail.com>
2022-02-13 15:32:47 +0300

    First kotlin assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val name = "Zuenah Wairimu"
+    println(name)
+    val age = "21"
+    println(age)
+    val weight = "50"
+    println(weight)
+    val iskenyan: Boolean =true
+    println(iskenyan) // output  true
+    val isnotkenyan :Boolean= false
+    println(isnotkenyan) // output false
+    val phonenumber: String = "0726789006"
+    println(phonenumber)
+}
